57d2dbc825f0a06f66639d463a00569f350479b6
unknown <HH545@ccs.local>
2026-04-28 20:46:28 +0800

    Initial commit

---
+++ src/app/_components/alert.component.ts
@@ -0,0 +1,6 @@
+import { ChangeDetectorRef, Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+import { Alert, AlertType } from '@app/_models';
+import { AlertService } from '@app/_services';
