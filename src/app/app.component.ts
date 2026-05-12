42f1c8e8b17388952c7599d550e3f66260a9514a
itsmejenicha <jenichajavier05@gmail.com>
2026-05-12 23:18:50 +0800

    Added new folders for the backend side

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+import { AccountService } from './_services';
+import { Account } from './_models';
+
+@Component({ 
+  selector: 'app-root', 
+  templateUrl: 'app.component.html',
+  standalone: false
 })
 export class AppComponent {
-  title = 'angular-21-boilerplate';
+  account?: Account | null;
+
+  constructor(private accountService: AccountService) {
+    this.accountService.account.subscribe(x => this.account = x);
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
 }
